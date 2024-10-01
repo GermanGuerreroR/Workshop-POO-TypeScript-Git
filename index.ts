3f55edff03b30646ef5f2c16b29da913b3e7495e
German G <german95guerrero@hotmail.com>
2024-09-30 22:46:20 -0500

    Ejercicio nro #7

--- index.ts
+++ index.ts
@@ -37,4 +37,26 @@ const empleado = new Empleado(persona.nombre, persona.getEdad(), 500000000);
 empleado.trabajar(6);
 
 // Punto nro # 6
+
 empleado.saludar();
+
+//Punto nro # 7
+
+const arrEmpleados: Empleado[] = [];
+
+const empleadoCero = new Empleado("Pepe Luis", 25, 5000);
+const empleadoUno = new Empleado("Luis Pepe", 26, 6500);
+const empleadoDos = new Empleado("Lucas Pepe", 27, 7540);
+const empleadoTres = new Empleado("Pepe Lucas", 28, 4781);
+const empleadoCuatro = new Empleado("Elba lazo", 29, 5984);
+const empleadoQuinto = new Empleado("Elza pato", 30, 5751);
+const empleadoSexto = new Empleado("Juancho Carrancho", 31, 5757);
+const empleadoSeptimo = new Empleado("Pepe Frijoles", 32, 4750);
+const empleadoOctavo = new Empleado("Thrall shaman supremo", 33, 5749);
+const empleadoNoveno = new Empleado("Elvis Tek", 34, 7899);
+
+arrEmpleados.push(empleadoCero, empleadoUno, empleadoDos, empleadoTres, empleadoCuatro, empleadoQuinto, empleadoSexto, empleadoSeptimo, empleadoOctavo, empleadoNoveno);
+
+for (let empleado of arrEmpleados) {
+    empleado.saludar();
+}
